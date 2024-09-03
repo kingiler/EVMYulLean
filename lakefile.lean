b234abd40d412b5d538d3c4b6fe37d81329e7b50
Frantisek Silvasi <silvasi.frantisek@gmail.com>
2024-09-03 11:09:30 +0200

    what on Earth

--- lakefile.lean
+++ lakefile.lean
@@ -4,13 +4,24 @@ open Lake DSL
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"@"f0957a7575317490107578ebaee9efaf8e62a4ab"
 
+require alloy from git "https://github.com/Ferinko/lean4-alloy.git"
+
 package «evmyul» {
   moreLeanArgs := #["-DautoImplicit=false"]
   moreServerOptions := #[⟨`DautoImplicit, false⟩]
 }
 
+module_data alloy.c.o.export : BuildJob FilePath
+module_data alloy.c.o.noexport : BuildJob FilePath
+
 @[default_target]
-lean_lib «EvmYul»
+lean_lib «EvmYul» where
+  precompileModules := true
+  nativeFacets := fun shouldExport =>
+    if shouldExport then
+      #[Module.oExportFacet, `alloy.c.o.export]
+    else
+      #[Module.oNoExportFacet, `alloy.c.o.noexport]
 
 lean_lib «Conform» 
 
